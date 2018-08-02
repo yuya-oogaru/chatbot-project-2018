ffa0ee7dd3a73165d2d3100db52f8c18f60afb2c
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 13:22:25 +0900

    1

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 $sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
 
+$inputString = file_get_contents('php://input');
+error_log($inputString);
+
 foreach ($events as $event) {
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
       !($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
