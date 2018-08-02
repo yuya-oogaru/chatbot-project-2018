9d046801b789cb0bb7d22d171f8588a5f7533f4b
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 13:07:19 +0900

    testbot

---
+++ backup/index.php
@@ -0,0 +1,9 @@
+<?php
+
+//composerでインストールしたライブラリ読み込み
+require_onec__DIR__ . '/vendor/autoload.php';
+
+$inputString = file_get_contents('php://input');
+error_log($inputString);
+
+?>
--- index.php
+++ index.php
@@ -3,7 +3,17 @@
 //composerでインストールしたライブラリ読み込み
 require_onec__DIR__ . '/vendor/autoload.php';
 
-$inputString = file_get_contents('php://input');
-error_log($inputString);
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+// LINE Messaging APIがリクエストに付与した署名を取得
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
+// 署名が正当かチェック。正当であればリクエストをパースし配列へ
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+// 配列に格納された各イベントをループで処理
+foreach ($events as $event) {
+	$bot->replyText($event->getReplyToken(), 'TextMessage');
+	}
 ?>
