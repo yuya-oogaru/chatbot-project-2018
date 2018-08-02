51834b827ec3d33745d45d2e959eba68efd82450
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 15:09:55 +0900

    s

--- index.php
+++ index.php
@@ -7,23 +7,12 @@ $inputString = file_get_contents('php://input');
 error_log($inputString);
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+error_log($signature);
 
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-$profResponse = $bot->getProfile('<userId>');
-if ($profResponse->isSucceeded()) {
-    $profile = $profResponse->getJSONDecodedBody();
-    echo $profile['displayName'];
-    echo $profile['pictureUrl'];
-    echo $profile['statusMessage'];
-}
 
 ?>
