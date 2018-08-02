71e8ce2c49b375318479f09d372c524da4cf7380
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 16:55:58 +0900

    Signed-off-by: yuya-oogaru <yuya-oogaru@upload-gp.co.jp>

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+echo '$httpClient';
 
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+echo '$bot';
 
 $inputData = file_get_contents('php://input');
 error_log($inputData);
