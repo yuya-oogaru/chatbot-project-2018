81a8cd472a4513a8d2cc57e5cecbc94aa6dff93c
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 13:28:06 +0900

    1

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-$sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
+$jsonString = file_get_contents('php://input');
+error_log($jsonString);
+$jsonObj = json_decode($jsonString);
 
-$inputString = file_get_contents('php://input');
-error_log($inputString);
 
-foreach ($events as $event) {
-  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
-      !($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
-    continue;
-  }
-  $bot->replyText($event->getReplyToken(), $event->getText());
+// 取得メッセージと送り返す先の指定
+$message = $jsonObj->{"events"}[0]->{"message"};
+$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+$messages =  $message->{"text"}
 }
+
+// 返送情報の作成と送信
+$response = [
+    'replyToken' => $replyToken,
+    'messages' => [$messageData,$messageData2]  // 複数送信時は、ここを増やすこと
+];
+
+?>
