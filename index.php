9b6d904c1fc76b65ed000a00633a8e615be33610
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 14:50:01 +0900

    1

--- index.php
+++ index.php
@@ -9,19 +9,9 @@ error_log($inputString);
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
-print "http client $httpClient
-";
-print "bot $bot
-";
-
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-
 $response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
 
-foreach ($events as $event) {
-
-    $bot->replyText($event->getReplyToken(), $event->getText());
-}
-
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 ?>
