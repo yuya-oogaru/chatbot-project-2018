5ae71949ac1092f5482e282200473ed16391466a
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 11:11:03 +0900

    change index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
-echo "Hello World!";
+//composerでインストールしたライブラリ読み込み
+require_onec__DIR__ . '/vendor/autoload.php';
+
+$inputString = file_get_contents('php://input');
+error_log($inputString);
 
 ?>
