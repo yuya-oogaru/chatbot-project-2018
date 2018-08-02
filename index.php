cce44c6303131ac8c1525f3214f07a8a52cf2925
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 15:43:14 +0900

    a

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 $inputString = file_get_contents('php://input');
-error_log($inputString);
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$signature = $_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE]; 
+$Events = $Bot->parseEventRequest($inputString, $Signature);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
 
