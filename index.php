82580ac8828abd1365bdd850055b12d0aa9ed9fd
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 17:04:11 +0900

    Signed-off-by: yuya-oogaru <yuya-oogaru@upload-gp.co.jp>

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-printf("client = %p\n",$httpClient);
+printf("client = %d\n",$httpClient);
 
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-printf("bot = %p\n",$bot);
+printf("bot = %d\n",$bot);
 
 $inputData = file_get_contents('php://input');
 error_log($inputData);
