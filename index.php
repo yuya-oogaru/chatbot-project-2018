a5e0f8f0097f6b87d665ed040a6922aa569de2a3
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 14:02:19 +0900

    a

--- index.php
+++ index.php
@@ -1,24 +1,47 @@
 <?php
 
+<?php
 //composerでインストールしたライブラリ読み込み
 require_once __DIR__.'/vendor/autoload.php';
 
 $inputString = file_get_contents('php://input');
 error_log($inputString);
 
-// アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-// LINE Messaging APIがリクエストに付与した署名を取得
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-// 署名が正当かチェック。正当であればリクエストをパースし配列へ
-$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-// 配列に格納された各イベントをループで処理
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+try {
+    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+}
+catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+    error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
+}
+catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+    error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
+}
+catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+    error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
+}
+catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+    error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
+}
+
+
 foreach ($events as $event) {
-	$bot->replyText($event->getReplyToken(), 'TextMessage');
-	}
-	
+    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+        error_log('Non message event has come');
+        continue;
+    }
+
+    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+        error_log('Non text message has come');
+        continue;
+    }
+
+    $bot->replyText($event->getReplyToken(), $event->getText());
+}
+
 ?>
 
