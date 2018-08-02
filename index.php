566d999122d445168a9bddea500803c3a03c99ac
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 11:52:23 +0900

    a

--- index.php
+++ index.php
@@ -3,16 +3,7 @@
 //composerでインストールしたライブラリ読み込み
 require_onec__DIR__ . '/vendor/autoload.php';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-
-$bot = new \LINE\LINEBot($httpClient, ['channnelSecret' => getenv('CHANNEL_SECRET')]);
-
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-$events = $bot->parseEventRequest(file_get_contrnts('php://input'),$signsture);
-
-foreach ($events as $event){
-	$bot->replyText($events->getReplyToken(), 'TextMessage');
-}
+$inputString = file_get_contents('php://input');
+error_log($inputString);
 
 ?>
