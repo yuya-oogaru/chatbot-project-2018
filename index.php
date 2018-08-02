6f949128ef68e052af96e6c9734c09e6cb69ec2f
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 15:56:11 +0900

    Signed-off-by: yuya-oogaru <yuya-oogaru@upload-gp.co.jp>

--- index.php
+++ index.php
@@ -3,17 +3,18 @@
 //composerでインストールしたライブラリ読み込み
 require_once __DIR__.'/vendor/autoload.php';
 
-$inputString = file_get_contents('php://input');
+$inputData = file_get_contents("php://input");
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 $signature = $_SERVER["HTTP_". \LINE\LINEBot\Constant\HTTPHeader :: LINE_SIGNATURE]; 
-$Events = $Bot->parseEventRequest($inputString, $Signature);
+$Events = $Bot->parseEventRequest($inputData, $signature);
 
 foreach($Events as $event){
 
+$SendMessage = new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
-
+$SendMessage->add($textMessageBuilder);
 $response = $bot -> replyMessage($event->getReplyToken(), $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
