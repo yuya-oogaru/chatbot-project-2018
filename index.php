bf9b21c225232088ba2b104021a666a98d05d29b
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-02 16:34:52 +0900

    Signed-off-by: yuya-oogaru <yuya-oogaru@upload-gp.co.jp>

--- index.php
+++ index.php
@@ -3,28 +3,11 @@
 // Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 
-// アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-// LINE Messaging APIがリクエストに付与した署名を取得
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-// 署名が正当かチェック。正当であればリクエストをパースし配列へ
-$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-// 配列に格納された各イベントを
-foreach ($events as $event) {
-$bot->replyText($event->getReplyToken(), 'TextMessage');
-}
-// テキストを返信。引数はLINEBot、返信先、テキスト
-function replyTextMessage($bot, $replyToken, $text) {
-  // 返信を行いレスポンスを取得
-  // TextMessageBuilderの引数はテキスト
-  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
-  // レスポンスが異常な場合
-  if (!$response->isSucceeded()) {
-    // エラー内容を出力
-    error_log('Failed! '. $response->getHTTPStatus . ' ' . $response->getRawBody());
-  }
-}
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+$inputData = file_get_contents('php://input');
+error_log($inputData);
+
 ?>
