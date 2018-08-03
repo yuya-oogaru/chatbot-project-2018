ddc4ff4ca4309a234ee58ca8e70d3a2d31c9f2c8
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-03 16:59:09 +0900

    Signed-off-by: yuya-oogaru <yuya-oogaru@upload-gp.co.jp>

--- index.php
+++ index.php
@@ -55,7 +55,6 @@ if($startPos != 'false'){
 			new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('入力された経路は['. substr($getMessage, 0, $routeNamePos). ']です。'),
 			new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('日付は['. substr($getMessage, $routeNamePos, 12). ']です。'),
 			new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($transitTimePos),
-			new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($totalPricePos),
 			new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('乗換回数は['. substr($getMessage, $transitTimePos, 12). ']です。')
 			//new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('運賃合計は['. substr($getMessage, $totalPricePos, 12). ']です。')
 		);
