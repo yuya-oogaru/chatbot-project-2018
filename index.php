c7d11314f32f61be6d13005faeed68d4eaa9ff26
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-03 16:13:37 +0900

    Signed-off-by: yuya-oogaru <yuya-oogaru@upload-gp.co.jp>

--- index.php
+++ index.php
@@ -45,14 +45,14 @@ $routeNamePos = strpos($getMessage, '  ');
 //$transitTimePos = mb_strpos($getMessage, '　');
 
 $preSendMessage = 'default text';
-
+new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($startPos);
 /*返信*/
 if($startPos != 'false'){
 	foreach ($events as $event) {
 		replyMultiMessage($bot, $replyToken, 
 			new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($routeNamePos),
-			new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($transitTimePos),
-			new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('入力された経路は['. substr($getMessage, 0, $routeNamePos). ']です。')
+			new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($transitTimePos)
+			//new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('入力された経路は['. substr($getMessage, 0, $routeNamePos). ']です。'),
 			//new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('日付は['. substr($getMessage, $routeNamePos, 12). ']です。'),
 			//new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('乗換回数は['. substr($getMessage, $transitTimePos, 12). ']です。'),
 			//new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('運賃合計は['. substr($getMessage, ($transitTimePos + 12), 12). ']です。')
