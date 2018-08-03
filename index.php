803c593f84933327e433ea0b308bb84db99caa65
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-03 09:37:41 +0900

    Signed-off-by: yuya-oogaru <yuya-oogaru@upload-gp.co.jp>

--- index.php
+++ index.php
@@ -9,13 +9,19 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
 // 署名が正当かチェック。正当であればリクエストをパースし配列へ
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+$getMessage = $json->events[0]->message->text;
+
+$replyToken = $json->events[0]->replyToken;
+
+$sendMessage =  new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($getMessage);
+
 // 配列に格納された各イベントをループで処理
 foreach ($events as $event) {
 
-  $response = $bot->replyMessage($json->events[0]->replyToken, $message = $json->events[0]->message->text);
+  $response = $bot->replyMessage($replyToken, $sendMessage);
   // レスポンスが異常な場合
   if (!$response->isSucceeded()) {
     // エラー内容を出力
