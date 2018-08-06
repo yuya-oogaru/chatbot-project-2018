8a6deb495b020a0cfb0303008b8651287f409487
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-08-06 09:45:31 +0900

    Signed-off-by: yuya-oogaru <yuya-oogaru@upload-gp.co.jp>

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if($startPos != false){
 		
 			new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('交通費データは以下の内容で登録可能です。
 			
-			'.'登録者名 : ['.$profile['displayName'].']'
+			'.'登録者名 : ['.$profile['displayName'].']
 			'.'経路 : ['.$routes.']
 			'.'日付 : ['.$date.']
 			'.'乗換回数 : ['.$transit.']回
