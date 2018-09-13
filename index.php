e3a476c0fe6e270a0578fae53c2b593bfca1b853
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-09-13 17:13:28 +0900

    k

--- index.php
+++ index.php
@@ -45,107 +45,75 @@ $post_data = [
 $post_data = [
 	"replyToken" => $reply_token,
 	"messages" => [
-[
-  "type"=> "bubble",
-  "styles"=> [
-    "footer"=> [
-      "separator"=> true
-    ]
-  ],
-  "body"=> [
-    "type"=> "box",
-    "layout"=> "vertical",
-    "contents"=> [
-      [
-        "type"=> "box",
-        "layout"=> "vertical",
-        "margin"=> "xxl",
-        "spacing"=> "sm",
-        "contents"=> [
-          [
-            "type"=> "text",
-            "text"=> "内容確認",
-            "weight"=> "bold",
-            "size"=> "xxl",
-            "margin"=> "md"
-          ],
-          [
-            "type"=> "separator",
-            "margin"=> "md"
-          ]
-        ]
-      ],
-      [
-        "type"=> "box",
-        "layout"=> "vertical",
-        "margin"=> "xxl",
-        "spacing"=> "sm",
-        "contents"=> [
-          [
-            "type"=> "box",
-            "layout"=> "horizontal",
-            "contents"=> [
-              [
-                "type"=> "text",
-                "text"=> "乗車日",
-                "size"=> "xxs",
-                "color"=> "#0000ff",
-                "flex"=> 0
-              ],
-              [
-                "type"=> "text",
-                "text"=> "9/20",
-                "size"=> "xs",
-                "color"=> "#111111",
-                "align"=> "end"
-              ]
-            ]
-          ],
-          [
-            "type"=> "separator",
-            "margin"=> "md"
-          ]
-        ]
-      ],
-      [
-        "type"=> "box",
-        "layout"=> "vertical",
-        "margin"=> "xxl",
-        "spacing"=> "sm",
-        "contents"=> [
-          [
-            "type"=> "text",
-            "text"=> "以上の内容で登録しますか？",
-            "size"=> "md",
-            "color"=> "#111111",
-            "align"=> "center"
-          ],
-          [
-            "type"=> "button",
-            "height"=> "md",
-            "style"=> "primary",
-            "action"=> [
-              "type"=> "uri",
-              "label"=> "はい",
-              "uri"=> "https://example.com"
-            ]
-          ],
-          [
-            "type"=> "button",
-            "height"=> "md",
-            "style"=> "secondary",
-            "action"=> [
-              "type"=> "uri",
-              "label"=> "いいえ",
-              "uri"=> "https://example.com"
-            ]
-          ]
-        ]
-      ]
-    ]
-  ]
-]
-]
+	[
+		"type"=> "bubble",
+		"styles"=> [
+			"footer"=> [
+				"separator"=> true
+			]
+  		],
+  		"body"=> [
+			"type"=> "box",
+    		"layout"=> "vertical",
+    		"contents"=> [
+      			[
+        			"type"=> "box",
+        			"layout"=> "vertical",
+        			"margin"=> "xxl",
+        			"spacing"=> "sm",
+        			"contents"=> [
+          				[
+            				"type"=> "text",
+            				"text"=> "内容確認",
+            				"weight"=> "bold",
+            				"size"=> "xxl",
+            				"margin"=> "md"
+          				],
+          				[
+            				"type"=> "separator",
+            				"margin"=> "md"
+          				]
+        			]
+      			],
+      			[
+        			"type"=> "box",
+        			"layout"=> "vertical",
+        			"margin"=> "xxl",
+        			"spacing"=> "sm",
+        			"contents"=> [
+          				[
+            				"type"=> "text",
+            				"text"=> "以上の内容で登録しますか？",
+            				"size"=> "md",
+            				"color"=> "#111111",
+            				"align"=> "center"
+          				],
+          				[
+            				"type"=> "button",
+            				"height"=> "md",
+            				"style"=> "primary",
+            				"action"=> [
+           						"type"=> "uri",
+              					"label"=> "はい",
+              					"uri"=> "https://example.com"
+            				]
+          				],
+          				[
+            				"type"=> "button",
+            				"height"=> "md",
+            				"style"=> "secondary",
+            				"action"=> [
+              					"type"=> "uri",
+              					"label"=> "いいえ",
+              					"uri"=> "https://example.com"
+            				]
+          				]
+        			]
+      			]
+    		]
+  		]
+	]
+	]
 ];
 
 /*************************************************/
