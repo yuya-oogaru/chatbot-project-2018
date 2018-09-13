9b6bedb3fa0b31c16a361475638d1fafca23c40e
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-09-13 16:57:47 +0900

    :

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <?php
  
-require_once __DIR__ . '/messageTemplate.php';
+//require_once __DIR__ . '/messageTemplate.php';
 
  
 $access_token = getenv('CHANNEL_ACCESS_TOKEN');
@@ -41,5 +41,36 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 $result = curl_exec($ch);
 curl_close($ch);
 
+function confirmTemplate($void){
+
+$post_data = [
+	"replyToken" => $reply_token,
+	"messages" => [
+	  [
+  		"type" => "template",
+  		"altText" => "this is a confirm template",
+  		"template" => [
+			"type" => "confirm",
+     	 	"text" => "Are you sure?",
+     	 	"actions" => [
+     	    	[
+            		"type" => "message",
+            		"label" => "Yes",
+            		"text" => "yes"
+          		],
+          		[
+           		 	"type" => "message",
+            		"label" => "No",
+            		"text" => "no"
+          		]
+      		]
+  		]
+  	  ]
+  	]
+];
+
+	return $post_data;
+
+}
 
 ?>
