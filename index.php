93895a15d2114d32f6351763622511e2fbaf4922
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-09-13 16:59:15 +0900

    s

--- index.php
+++ index.php
@@ -15,34 +15,6 @@ $json_obj = json_decode($json_string);
 $message = $json_obj->{"events"}[0]->{"message"};
 $reply_token = $json_obj->{"events"}[0]->{"replyToken"};
 
-$post_data = confirmTemplate(1);
-/*
-//ユーザーからのメッセージに対し、オウム返しをする
-$post_data = [
-  "replyToken" => $reply_token,
-  "messages" => [
-    [
-      "type" => "text",
-      "text" => $message->{"text"}
-    ]
-  ]
-];
- */
-//curlを使用してメッセージを返信する
-$ch = curl_init("https://api.line.me/v2/bot/message/reply");
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json; charser=UTF-8',
-    'Authorization: Bearer ' . $access_token
-));
-$result = curl_exec($ch);
-curl_close($ch);
-
-function confirmTemplate($void){
-
 $post_data = [
 	"replyToken" => $reply_token,
 	"messages" => [
@@ -69,8 +41,17 @@ $post_data = [
   	]
 ];
 
-	return $post_data;
-
-}
+//curlを使用してメッセージを返信する
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json; charser=UTF-8',
+    'Authorization: Bearer ' . $access_token
+));
+$result = curl_exec($ch);
+curl_close($ch);
 
 ?>
