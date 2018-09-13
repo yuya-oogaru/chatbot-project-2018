aa87aec891f688bdb22512951d8c61d333b12fe8
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-09-13 14:56:24 +0900

    1

--- index.php
+++ index.php
@@ -13,6 +13,12 @@ $json_obj = json_decode($json_string);
 $message = $json_obj->{"events"}[0]->{"message"};
 $reply_token = $json_obj->{"events"}[0]->{"replyToken"};
  
+/*てすとー*/
+$post_data = json_decode($json_template);
+
+$post_data->{"replyToken"} = $reply_token
+ 
+ /*
 //ユーザーからのメッセージに対し、オウム返しをする
 $post_data = [
   "replyToken" => $reply_token,
@@ -23,13 +29,13 @@ $post_data = [
     ]
   ]
 ];
- 
+ */
 //curlを使用してメッセージを返信する
 $ch = curl_init("https://api.line.me/v2/bot/message/reply");
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $json_template);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json; charser=UTF-8',
     'Authorization: Bearer ' . $access_token
