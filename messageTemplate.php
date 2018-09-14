5bc3862f38339778632eef07f544a1570c3935d5
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-09-14 09:42:16 +0900

    j

--- messageTemplate.php
+++ messageTemplate.php
@@ -3,9 +3,7 @@
 //確認テンプレート
 function confirmTemplate($reply_token){
 
-error_log('reply_token = '.$reply_token.'');
-
-$confirm = [
+return [
 	"replyToken" => $reply_token,
 	"messages" => [
 	  [
@@ -31,203 +29,20 @@ $confirm = [
   	]
 ];
 
-error_log('cfTemp = '.json_encode($confirm).'');
-
 return $confirm;
 
 }
 //Flexテンプレート
 function FlexTemplate($reply_token){
 
-error_log('reply_token = '.$reply_token.'');
-
 $flexmsg = [
 	"replyToken" => $reply_token,
 	"messages" => [
-	[
-		"type" => "flex",
-		"altText" => "test",
-		"contents" => [
-			"type"=> "bubble",
-			"styles"=> [
-				"footer"=> [
-					"separator"=> true
-				]
-  			],
-  			"body"=> [
-				"type"=> "box",
-    			"layout"=> "vertical",
-    			"contents"=> [
-      				[
-        				"type"=> "box",
-        				"layout"=> "vertical",
-        				"margin"=> "xxl",
-        				"spacing"=> "sm",
-        				"contents"=> [
-          					[
-            					"type"=> "text",
-            					"text"=> "内容確認",
-            					"weight"=> "bold",
-            					"size"=> "xxl",
-            					"margin"=> "md"
-          					],
-          					[
-            					"type"=> "separator",
-            					"margin"=> "md"
-          					]
-        				]
-      				],
-      				[
-        				"type"=> "box",
-        				"layout"=> "vertical",
-        				"margin"=> "xxl",
-        				"spacing"=> "sm",
-        				"contents"=> [
-          					[
-            					"type"=> "box",
-            					"layout"=> "horizontal",
-            					"contents"=> [
-              						[
-                						"type"=> "text",
-                						"text"=> "乗車日",
-                						"size"=> "xxs",
-                						"color"=> "#0000ff",
-                						"flex"=> 0
-              						],
-              						[
-                						"type"=> "text",
-                						"text"=> "9/20",
-                						"size"=> "xs",
-                						"color"=> "#111111",
-                						"align"=> "end"
-              						]
-            					]
-          					],
-          					[
-            					"type"=> "box",
-            					"layout"=> "horizontal",
-            					"contents"=> [
-              						[
-                						"type"=> "text",
-                						"text"=> "行先",
-                						"size"=> "xxs",
-                						"color"=> "#0000ff",
-                						"flex"=> 0
-              						],
-              						[
-                						"type"=> "text",
-                						"text"=> "OO精工株式会社",
-                						"size"=> "xs",
-                						"color"=> "#111111",
-                						"align"=> "end"
-              						]
-            					]
-          					],
-          					[
-            					"type"=> "box",
-            					"layout"=> "horizontal",
-            					"contents"=> [
-              						[
-                						"type"=> "text",
-                						"text"=> "経路",
-                						"size"=> "xxs",
-                						"color"=> "#0000ff",
-                						"flex"=> 0
-              						],
-              						[
-                						"type"=> "text",
-                						"text"=> "大阪～天王寺",
-                						"size"=> "xs",
-                						"color"=> "#111111",
-                						"align"=> "end"
-              						]
-            					]
-          					],
-          					[
-            					"type"=> "box",
-            					"layout"=> "horizontal",
-            					"contents"=> [
-              						[
-                						"type"=> "text",
-                						"text"=> "合計運賃",
-                						"size"=> "xxs",
-                						"color"=> "#0000ff",
-                						"flex"=> 0
-              						],
-              						[
-                						"type"=> "text",
-                						"text"=> "140円",
-                						"size"=> "xs",
-                						"color"=> "#111111",
-                						"align"=> "end"
-              						]
-            					]
-          					],
-          					[
-            					"type"=> "box",
-            					"layout"=> "horizontal",
-            					"contents"=> [
-              						[
-                						"type"=> "text",
-                						"text"=> "備考",
-                						"size"=> "xxs",
-                						"color"=> "#0000ff",
-                						"flex"=> 0
-              						],
-              						[
-                						"type"=> "text",
-                						"text"=> "往復",
-                						"size"=> "xs",
-                						"color"=> "#111111",
-                						"align"=> "end"
-              						]
-            					]
-          					],
-          					[
-            					"type"=> "separator",
-            					"margin"=> "md"
-          					]
-        				]
-      				],
-      				[
-        				"type"=> "box",
-        				"layout"=> "vertical",
-        				"margin"=> "xxl",
-        				"spacing"=> "sm",
-        				"contents"=> [
-          					[
-            					"type"=> "text",
-            					"text"=> "以上の内容で登録しますか？",
-            					"size"=> "md",
-            					"color"=> "#111111",
-            					"align"=> "center"
-          					],
-          					[
-            					"type"=> "button",
-           						"height"=> "md",
-            					"style"=> "primary",
-            					"action"=> [
-           							"type"=> "uri",
-              						"label"=> "はい",
-              						"uri"=> "https://example.com"
-            					]
-          					],
-          					[
-            					"type"=> "button",
-            					"height"=> "md",
-            					"style"=> "secondary",
-            					"action"=> [
-              						"type"=> "uri",
-              						"label"=> "いいえ",
-              						"uri"=> "https://example.com"
-            					]
-          					]
-        				]
-      				]
-    			]
-  			]
+		[
+			"type" => "flex",
+			"altText" => "test",
+			"contents" => []
 		]
-	]
 	]
 ];
 
@@ -236,5 +51,23 @@ error_log('fxTemp = '.json_encode($flexmsg).'');
 return $flexmsg;
 
 }
-?>
+//Flexバブル
+function FlexTemplateBubble($void){
 
+return[
+];
+
+}
+//Flexコンテンツ
+function FlexTemplateContents($void){
+
+return[
+	[
+	],
+	[
+	],
+];
+
+}
+
+?>
