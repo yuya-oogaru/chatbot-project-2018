a4e95637959ed3960ccda0062dd4552dcc1d18af
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-09-19 11:13:03 +0900

    s

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $userID = $json_obj->{"events"}[0]->{"source"}->{"userId"};
 
 /*************ユーザーのステータス確認******************/
 
-/*既存のユーザー情報がない場合*/
+/*既存のユーザー情報がない場合の初期化*/
 if(searchUserID($userID) == NULL){
 	registerUser($userID, 'pre_proc', 1);
 }
@@ -34,6 +34,100 @@ if(searchUserID($userID) == NULL){
 /*ステータス確認*/
 $status = searchStatus($userID);
 
+/****************ステータスに応じた処理呼び出し******************
+
+確認したステータスをもとに、当該の処理を呼び出し。
+
+
+***各状態名***
+
+[共通]
+
+pre_proc        :ユーザーが何も操作を行っていない状態（初期）
+
+[メニュー選択中]
+
+menu            :ユーザーがメニューを表示し、機能の選択を行っている状態
+
+[経路データ登録処理中]
+
+ins_inp_office  :ユーザーが、登録する行先名を入力している状態
+ins_sel_claim   :ユーザーが、ユーザー請求の可否を選択している状態
+ins_sel_rounds  :ユーザーが、往復の有無を選択している状態
+ins_inp_others  :ユーザーが、備考の入力を行っている状態
+ins_sel_confirm :ユーザーが、登録内容の確認を行っている状態
+
+[経路データ申請処理中]
+
+aplly_confirm   :ユーザーが申請内容の確認を行っている状態
+
+
+[経路データ削除処理中]
+
+del_inp_num     :ユーザーが削除するデータを選択している状態。
+del_confirm     :ユーザーが削除内容の確認を行っている状態。
+
+****************************************************************/
+
+switch($status){
+	case 'pre_proc':
+		
+		updateStatus($userID, 'ins_inp_office');
+		$post_data = textMessage($reply_token, 'ステータスをins_inp_officeへ移行');
+		
+		break;
+	case 'menu':
+		
+		break;
+	case 'ins_inp_office':
+		
+		updateStatus($userID, 'ins_sel_claim');
+		$post_data = textMessage($reply_token, 'ステータスをins_sel_claimへ移行');
+		
+		break;
+	case 'ins_sel_claim':
+		
+		updateStatus($userID, 'ins_sel_rounds');
+		$post_data = textMessage($reply_token, 'ステータスをins_sel_roundsへ移行');
+		
+		break;
+	case 'ins_sel_rounds':
+		
+		updateStatus($userID, 'ins_inp_others');
+		$post_data = textMessage($reply_token, 'ステータスをins_inp_othersへ移行');
+		
+		break;
+	case 'ins_inp_others':
+		
+		updateStatus($userID, 'ins_sel_confirm');
+		$post_data = textMessage($reply_token, 'ステータスをins_sel_confirmへ移行');
+		
+		break;
+	case 'ins_sel_confirm':
+		
+		updateStatus($userID, 'pre_proc');
+		$post_data = textMessage($reply_token, 'ステータスをpre_procへ移行');
+		
+		break;
+	case 'aplly_confirm':
+		
+		$post_data = textMessage($reply_token, 'ステータスエラー 管理者に報告してください');
+		
+		break;
+	case 'del_inp_num':
+		
+		$post_data = textMessage($reply_token, 'ステータスエラー 管理者に報告してください');
+		
+		break;
+	case 'del_confirm':
+		
+		$post_data = textMessage($reply_token, 'ステータスエラー 管理者に報告してください');
+		
+		break;
+	default:
+		$post_data = textMessage($reply_token, 'ステータスエラー 管理者に報告してください');
+		break;
+}
 /*****************応答メッセージ作成**********************/
 
 $post_data = textMessage($reply_token, $status);
---
+++ proc_launcher.php
@@ -0,0 +1,76 @@
+<?php
+
+/*以下の関数は、ユーザーの状態に応じて呼び出され、それに合わせた処理を実行させる*/
+
+
+/*ユーザーが何も操作を行っていない状態*/
+function state_pre_proc(){
+
+
+
+
+}
+/*ユーザーがメニューを表示し、機能の選択を行っている状態*/
+function state_menu(){
+
+
+
+
+}
+/*ユーザーが、登録する行先名を入力している状態*/
+function state_ins_inp_office(){
+
+
+
+
+}
+/*ユーザーが、ユーザー請求の可否を選択している状態*/
+function state_ins_sel_claim(){
+
+
+
+
+}
+/*ユーザーが、往復の有無を選択している状態*/
+function state_ins_sel_rounds(){
+
+
+
+
+}
+/*ユーザーが、備考の入力を行っている状態*/
+function state_ins_inp_others(){
+
+
+
+
+}
+/*ユーザーが、登録内容の確認を行っている状態*/
+function state_ins_sel_confirm(){
+
+
+
+
+}
+/*ユーザーが申請内容の確認を行っている状態*/
+function state_aplly_confirmc(){
+
+
+
+
+}
+/*ユーザーが削除するデータを選択している状態。*/
+function state_del_inp_num(){
+
+
+
+
+}
+/*ユーザーが削除内容の確認を行っている状態。*/
+function state_del_confirm(){
+
+
+
+
+}
+?>
