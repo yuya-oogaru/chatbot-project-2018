71bba20e369aa0be238ce0ae3173ef5a46967d3a
yuya-oogaru <yuya-oogaru@upload-gp.co.jp>
2018-09-19 09:59:58 +0900

    1

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $reply_token = $json_obj->{"events"}[0]->{"replyToken"};
 
 $post_data = textMessage($reply_token, 'default message');   /*応答フォーマット初期化*/
 
-function testSelection(1);
+testSelection(1);
 
 /*Jsonを日本語（２バイト文字）に対応 = json扱うファイルは 文字コードをUTF-8にしないといけない！！！*/
 
